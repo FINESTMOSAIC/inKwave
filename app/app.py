eb607f0efb79ffd1b8056d5f8eef5c54c770806e
FINESTMOSAIC <agrim@kulshreshtha.in>
2024-09-05 00:26:10 +0530

    deployment

--- app/app.py
+++ app/app.py
@@ -136,5 +136,5 @@ def toggle_hand_recognition(status):
 def about():
     return render_template('about.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+    # app.run(debug=True)
