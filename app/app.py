84d9b559f7da6ac6e56f90749c0d540066b27bcb
FINESTMOSAIC <agrim@kulshreshtha.in>
2024-09-05 13:14:37 +0530

    Port addede

--- app/app.py
+++ app/app.py
@@ -147,5 +147,5 @@ def toggle_hand_recognition(status):
 def about():
     return render_template('about.html')
 
-# if __name__ == "__main__":
-    # app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8001, debug=True)
